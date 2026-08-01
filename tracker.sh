1c45347b155468bc01abfa3ab7480acbdb65d881
Harshith-76 <harshithacharya15@gmail.com>
2026-08-01 11:57:37 +0000

    Add AWS CLI validation

--- tracker.sh
+++ tracker.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
+# ==========================================
+# AWS Resource Monitor & Automation Toolkit
+# Version: 1.0.0
+# Author : Harshith Acharya
+# ==========================================
+
 clear
 
-echo "========================================"
-echo "      AWS RESOURCE MONITOR"
-echo "========================================"
+echo "======================================================="
+echo "       AWS RESOURCE MONITOR & AUTOMATION TOOLKIT"
+echo "======================================================="
+echo "Version : 1.0.0"
+echo "Author  : Harshith Acharya"
+echo "======================================================="
 echo ""
+
+echo "[INFO] Checking AWS CLI installation..."
+
+if command -v aws >/dev/null 2>&1
+then
+    echo "[SUCCESS] AWS CLI is installed."
+else
+    echo "[ERROR] AWS CLI is not installed."
+    exit 1
+fi
+
+echo ""
+echo "Press Enter to continue..."
+read
